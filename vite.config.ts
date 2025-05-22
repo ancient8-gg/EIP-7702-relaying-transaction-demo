64fb7ebd78354753d2e0bbe9ec315a90fb5c146b
Richard <thanhle@ancient8.gg>
2025-05-22 13:37:50 +0700

    fix: add base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/EIP-7702-relaying-transaction-demo/',
   plugins: [react(), tailwindcss()],
 });
